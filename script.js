16fd2e4a76c4d13e98d9876b916e7172a91379ef
tp-app <t.phelut@gmail.com>
2025-07-28 22:54:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Spendy app ready');
+
+// Exemple simplifié
+function downloadPDF() {
+  alert("Fonction PDF simulée.");
+}
+
+document.getElementById('participants-section').innerHTML = '<p>Participants chargés dynamiquement ici</p>';
+document.getElementById('expenses-section').innerHTML = '<p>Interface dépenses ici</p>';
+document.getElementById('summary-section').innerHTML = '<p>Résumé calculé ici</p>';
